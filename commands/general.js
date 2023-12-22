6b5531a61086684b7866567c53615e37c47a4350
starkoka <j22437@inc.kisarazu.ac.jp>
2023-12-22 10:09:40 +0900

    feat:aboutコマンド実装

--- commands/general.js
+++ commands/general.js
@@ -14,5 +14,49 @@ module.exports =
             async execute(interaction) {
                 await interaction.reply( `Ping : ${interaction.client.ws.ping}ms` );
             },
-        }
+        },
+        {
+            data: new SlashCommandBuilder()
+                .setName('about')
+                .setDescription('このBOTの概要を表示します'),
+            async execute(interaction) {
+                const embed = new EmbedBuilder()
+                    .setColor(0x00A0EA)
+                    .setTitle('embedTwitter概要')
+                    .setAuthor({
+                        name: "embedTwitterOnDiscord",
+                        iconURL: 'https://avatars.githubusercontent.com/u/103174676',
+                        url: 'https://github.com/starkoka/embedTwitterOnDiscord'
+                    })
+                    .setDescription('このbotの概要を紹介します')
+                    .addFields(
+                        [
+                            {
+                                name: 'バージョン情報',
+                                value: 'v' + packageVer.version,
+                            },
+                            {
+                                name: '開発者',
+                                value: '[kokastar](https://github.com/starkoka)',
+                            },
+                            {
+                                name:"ソースコード",
+                                    value:"このBOTは、オープンソースとなっています。[GitHub](https://github.com/starkoka/embedTwitterOnDiscord)にて公開されています。\n"
+                            },
+                            {
+                                name:"バグの報告先",
+                                value:"[Issue](https://github.com/starkoka/embedTwitterOnDiscord/issues)までお願いします。\nサポート等の詳細は/helpを実行してください。\n"
+                            },
+                            {
+                                name: '実行環境',
+                                value: 'node.js v' + process.versions.node + `\n discord.js v` + version + ``,
+
+                            },
+                        ]
+                    )
+                    .setTimestamp()
+                    .setFooter({ text: 'Developed by kokastar' });
+                await interaction.reply({ embeds: [embed] });
+            },
+        },
 ]
